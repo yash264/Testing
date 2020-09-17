2ae9bf9a860b100fb2cb5eda4e8d63ed3b60619e
yash264 <yashbansal264@gmail.com>
2020-09-17 14:38:38 +0530

    *

---
+++ src/main/java/com/pkb/Test/MyThread.java
@@ -0,0 +1,13 @@
+package com.pkb.Test;
+
+public class MyThread extends Thread{
+	
+	public void run() {
+		
+		for (int i=0;i<=9;i++) System.out.println("yash.. be patient.. you are going to be great");
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/com/pkb/Test/Td.java
@@ -0,0 +1,15 @@
+package com.pkb.Test;
+
+public class Td {
+
+	public static void main(String[] args) {
+		
+		MyThread mt = new MyThread();
+		mt.start();
+		
+		for (int i=0;i<=9;i++) System.out.println("yash.. be patient.. you are going to be great -- I know that");
+		
+
+	}
+
+}
